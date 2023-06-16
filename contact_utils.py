eeddeb8e7f580c1f1ceebec598d2d48a7d1510b5
af0liveira <af0liveira@icloud.com>
2023-06-16 14:39:37 +0200

    Implemented `csv_to_contacts()` in `contact_utils.py`.
    The function converts a contacts list from CSV into a list of `Contact`
    objects.
    This list can be serialized with pickle, json, or other format.
    
    The `main.py` file was modified to use `csv_to_contacts()` to retrieve
    the data form the CSV file as a list of `Contact` instances.

--- contact_utils.py
+++ contact_utils.py
@@ -1,7 +1,20 @@
 """Module with tools for managing Contact objects."""
+import csv
+from contact import Contact
 
-def csv_to_contacts():
-    raise NotImplementedError(f"Function {__name__} hasn't been implemented yet.")
+def csv_to_contacts(csv_path, encoding='latin_1'):
+    """Converts a contact list from CSV to a list of Contact objects."""
+
+    contacts = []
+
+    with open(csv_path, mode='r', encoding=encoding) as fp:
+        csv_reader = csv.reader(fp)
+        for line in csv_reader:
+            id, name, email = line
+            contact = Contact(id=id, name=name, email=email)
+            contacts.append(contact)
+
+    return contacts
 
 def contacts_to_pickle():
     raise NotImplementedError(f"Function {__name__} hasn't been implemented yet.")
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
+from contact_utils import csv_to_contacts
+
 csv_path = './data/contacts.csv'
 
 try:
-    with open(csv_path, 'r', encoding='latin_1') as fp:
-        for contact in fp:
-            print(contact, end='')
+    contacts = csv_to_contacts(csv_path)
 except FileNotFoundError:
     print(f"File '{csv_path}' could not be found!")
 except PermissionError:
     print(f"You don't have the required permissions to open `{csv_path}`!")
+
+for contact in contacts:
+    print(contact.__dict__)
