a3025d5566e2e048a49b8bcd692d1308a82ca7c3
af0liveira <af0liveira@icloud.com>
2023-06-16 16:03:05 +0200

    Implemented `contacts_to_json()` and `json_to_contacts()`.
    The methods server for serialization and deserialization of contacts
    list using to and from JSON files.
    
    The `main.py` was modified to utilize these functions to write and read
    the JSON file.
    
    Since JSON doesn't offer the risks of pickle, the generated JSON has
    been included in the repo.
    
    Also removed a few blank lines from the code, as they weren't really
    making it easier to read the code.

--- contact_utils.py
+++ contact_utils.py
@@ -1,41 +1,45 @@
 """Module with tools for managing Contact objects."""
 
 import csv
+import json
 import pickle
 
 from contact import Contact
 
 
-def csv_to_contacts(csv_path, encoding='latin_1'):
+def csv_to_contacts(filepath, encoding='latin_1'):
     """Converts a contact list from CSV to a list of Contact objects."""
-
     contacts = []
-
-    with open(csv_path, mode='r', encoding=encoding) as fp:
+    with open(filepath, mode='r', encoding=encoding) as fp:
         csv_reader = csv.reader(fp)
         for line in csv_reader:
             id, name, email = line
             contact = Contact(id=id, name=name, email=email)
             contacts.append(contact)
-
     return contacts
 
 def contacts_to_pickle(contacts, filepath):
     """Serialize a list of Contact objects to a pickle file."""
-
     with open(filepath, 'wb') as fp:
         pickle.dump(contacts, fp)
 
 def pickle_to_contacts(filepath):
     """Deserialize a contacts list from a pickle file."""
-
     with open(filepath, 'rb') as fp:
         contacts = pickle.load(fp)
-
     return contacts
 
-def contacts_to_json():
-    raise NotImplementedError(f"Function {__name__} hasn't been implemented yet.")
+def contacts_to_json(contacts, filepath):
+    """Serialize a contacts lists to JSON format."""
+    with open(filepath, 'w') as fp:
+        json.dump(contacts, fp, default=lambda contact: contact.__dict__)
 
-def json_to_contacts():
-    raise NotImplementedError(f"Function {__name__} hasn't been implemented yet.")
+def json_to_contacts(filepath):
+    """Deserialize a contacts list in JSON format."""
+    contacts = []
+    with open(filepath, 'r') as fp:
+        contacts_json = json.load(fp)
+        for contact in contacts_json:
+            c = Contact(**contact)
+            contacts.append(c)
+    return contacts
--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python3
 
-from contact_utils import (csv_to_contacts, contacts_to_pickle,
-                           pickle_to_contacts)
+from contact_utils import (csv_to_contacts,
+                           contacts_to_pickle, pickle_to_contacts,
+                           contacts_to_json, json_to_contacts)
 
 csv_path = './data/contacts.csv'
 pickle_path = './data/contacts.p'
+json_path = './data/contacts.json'
 
 try:
     contacts = csv_to_contacts(csv_path)
     contacts_to_pickle(contacts, pickle_path)
+    contacts_to_json(contacts, json_path)
 except FileNotFoundError:
     print(f"File '{csv_path}' could not be found!")
 except PermissionError:
     print(f"You don't have the required permissions to open `{csv_path}`!")
 
+print("Contacts from pickle:")
 for contact in pickle_to_contacts(pickle_path):
+    print(contact.__dict__)
+
+print("Contacts from JSON:")
+for contact in json_to_contacts(json_path):
     print(contact.__dict__)
