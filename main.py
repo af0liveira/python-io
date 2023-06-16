4f1ceb1badb76de973cfa060649fd2b436c2e1b0
af0liveira <af0liveira@icloud.com>
2023-06-16 13:48:54 +0200

    Modified main.py for always close the file if there's an exception.
    Using a try-finally block solves the problem when there are errors
    during reading.
    However, if the exception is triggered by the `open` command, the
    `finally` block will try to close an unopened file, throwing yet another
    exception unrelated to the actual problem.

--- main.py
+++ main.py
@@ -2,9 +2,11 @@
 
 csv_path = './data/contacts.csv'
 
-fp = open(csv_path, 'r', encoding='latin_1')
+try:
+    fp = open(csv_path, 'r', encoding='latin_1')
 
-for contact in fp:
-    print(contact, end='')
+    for contact in fp:
+        print(contact, end='')
 
-fp.close()
+finally:
+    fp.close()
