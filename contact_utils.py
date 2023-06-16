14d73ba83c48a411c668e7bb752b49d36d52f89f
af0liveira <af0liveira@icloud.com>
2023-06-16 14:21:01 +0200

    Created contact_utils.py.
    The module `contact_utils` will implement methods for handling `Contact`
    objects and lists thereof.
    
    Currently, `contact_utils` only contains the function names, which will
    throw a `NotImplementedError` exception if called.

---
+++ contact_utils.py
@@ -0,0 +1,16 @@
+"""Module with tools for managing Contact objects."""
+
+def csv_to_contacts():
+    raise NotImplementedError(f"Function {__name__} hasn't been implemented yet.")
+
+def contacts_to_pickle():
+    raise NotImplementedError(f"Function {__name__} hasn't been implemented yet.")
+
+def pickle_to_contacts():
+    raise NotImplementedError(f"Function {__name__} hasn't been implemented yet.")
+
+def contacts_to_json():
+    raise NotImplementedError(f"Function {__name__} hasn't been implemented yet.")
+
+def json_to_contacts():
+    raise NotImplementedError(f"Function {__name__} hasn't been implemented yet.")
